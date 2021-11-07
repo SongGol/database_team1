0adab8f929cf4b406950ae7d2306a2394aa38d30
SongGol <dldmsgus2@gmail.com>
2021-11-07 20:54:13 +0900

    E_Insert 수정

--- src/database/E_Insert.java
+++ src/database/E_Insert.java
@@ -7,10 +7,15 @@ public class E_Insert extends Insert{
         Connector conn = new Connector();
         conn.connect();
         String query = String.format("INSERT INTO `EMPLOYEE` (`Fname`, `Minit`, `Lname`, `Ssn`, `Bdate`, `Address`, `Sex`, `Salary`, `Super_ssn`, `Dno`)" +
-                " VALUES ('%s','%s','%s','%s','%s', '%s', '%s', %s, %s, %s)",Fname,Minit,Lname,Ssn,Bdate,Address, Sex,Salary,Super_ssn,Dno);
+                " VALUES ('%s','%s','%s','%s','%s', '%s', '%s', %s, %s, %s)",Fname,Minit,Lname,Ssn,Bdate,Address, Sex,Salary,"NULL",Dno);
+        String update_SS_query = "UPDATE EMPLOYEE SET Super_ssn = "+Super_ssn+" WHERE Ssn LIKE '"+Ssn+"'";
         System.out.println(query);
+        System.out.println(update_SS_query);
         try {
             boolean rs = conn.modify(query);
+            if(!Super_ssn.equals("NULL")){
+                conn.modify(update_SS_query);
+            }
             System.out.println("Insert 성공");
         }catch (Exception e){
             e.printStackTrace();
